9021081f9493d7406a382c3d98f158c9ad46774a
AaronStangz <Aaronstangline.as@gmail.com>
2024-09-21 17:19:20 -0400

    0.0.1

---
+++ Cowboy Bebop Type Game/Assets/Scripts/Flying.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public float spped;
+    public float bootsSpeed;
+    public float leftSpeed; 
+    public float rightSpeed;
+    public float lookSpeed;
+    public float boots;
+    public bool bootsAble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+}
